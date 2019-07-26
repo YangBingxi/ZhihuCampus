a13db6e8208a04bc90a24149e63627466bec50a3
youngsw <sw_young@outlook.com>
2019-07-26 16:15:14 +0800

    修改一些命名

--- src/main/java/edu/njupt/sw/configuration/CampusZhiHuWebConfiguration.java
+++ src/main/java/edu/njupt/sw/configuration/CampusZhiHuWebConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 
 @Component
-public class WendaWebConfiguration extends WebMvcConfigurerAdapter {
+public class CampusZhiHuWebConfiguration extends WebMvcConfigurerAdapter {
     @Autowired
     PassportInterceptor passportInterceptor;
 

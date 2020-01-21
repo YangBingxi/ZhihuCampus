9d8a38eee6baa4b5bbf4c6e009264e8cf117ddba
youngsw <sw_young@outlook.com>
2020-01-22 00:08:50 +0800

    change image source to qiniu

--- src/main/java/edu/njupt/sw/service/UserService.java
+++ src/main/java/edu/njupt/sw/service/UserService.java
@@ -55,7 +55,7 @@ public class UserService {
         user = new User();
         user.setName(username);
         user.setSalt(UUID.randomUUID().toString().substring(0, 5));
-        String head = String.format("http://images.nowcoder.com/head/%dt.png", new Random().nextInt(1000));
+        String head = String.format("http://q4auui9hp.bkt.clouddn.com/headUrl2/HeadImg%dsw.png", new Random().nextInt(1000));
         user.setHeadUrl(head);
         user.setPassword(CampusZhiHuUtil.MD5(password + user.getSalt()));
         userDAO.addUser(user);
